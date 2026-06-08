9fc082bcb8a3291d6d286e337ccbca6b6b0254f0
Deepak <darkdeepak001@gmail.com>
2026-06-08 15:06:01 +0530

    Remove overlay and popup display functions

--- script.js
+++ script.js
@@ -1,12 +1,1 @@
-var overlay = document.querySelector(".overlay")
-var popup = document.querySelector(".popup")
-function show()
-{
-    overlay.style.display="block"
-    popup.style.display="block"
-}
-function closepop()
-{
-    overlay.style.display="none"
-    popup.style.display="none"
-}
+
