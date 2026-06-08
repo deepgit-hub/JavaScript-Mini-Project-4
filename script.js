176362bce6670b14ae17d1a611baaf179810b614
Deepak <darkdeepak001@gmail.com>
2026-06-08 10:25:26 +0530

    Add functions to show and close popup

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-
+var overlay = document.querySelector(".overlay")
+var popup = document.querySelector(".popup")
+function show()
+{
+    overlay.style.display="block"
+    popup.style.display="block"
+}
+function closepop()
+{
+    overlay.style.display="none"
+    popup.style.display="none"
+}
